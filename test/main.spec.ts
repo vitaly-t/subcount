5d383f0d1a4e34ac0a2d11d611e05acc5f1d9aa5
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2019-05-30 22:51:40 +0100

    adding tests

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -4,13 +4,26 @@ import {CountedObservable, ISubCounts, Observable} from '../src';
 describe('Observable', () => {
     it('must notify about subscriptions', () => {
         const a = new Observable<number>();
-        const cb = (data: number) => {
-        };
+        const cb = () => 1;
         const s = chai.spy(cb);
         a.subscribe(s);
         a.next(123, (count: number) => {
             expect(count).to.be.equal(1);
             expect(s).to.have.been.called.with(123);
+        });
+    });
+    it('must limit notifications according to the max option', () => {
+        const a = new Observable<number>({max: 1});
+        const cb1 = () => 1;
+        const cb2 = () => 2;
+        const s1 = chai.spy(cb1);
+        const s2 = chai.spy(cb2);
+        a.subscribe(s1);
+        a.subscribe(s2);
+        a.next(123, (count: number) => {
+            expect(count).to.be.equal(1);
+            expect(s1).to.have.been.called.with(123);
+            expect(s2).to.not.have.been.called;
         });
     });
 });
