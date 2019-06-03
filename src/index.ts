3665418e652f41d21058ce3e112fae35d6e0bb3f
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2019-06-03 12:28:46 +0100

    removing the code

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export {Subscription} from './subscription';
-export {Observable, IObservableOptions, ISubscriber, SubFunction} from './observable';
-export {CountedObservable, ICountedOptions, ISubCounts} from './counted';
